abe701cb5f32f0678c7340f1d1c7b3f64d265714
Richard <Richard@Richard-PC>
2013-11-23 16:24:54 -0500

    The first setup of the database.

---
+++ src/studentdatabase/StudentDatabase.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentdatabase;
+
+/**
+ *
+ * @author Richard
+ */
+public class StudentDatabase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
